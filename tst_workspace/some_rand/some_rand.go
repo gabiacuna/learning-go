71c17eb855ecf32527125c95774457bb96340599
gabiacuna <gabriela.acuna@usm.cl>
2022-04-13 11:19:37 -0400

    chin

--- tst_workspace/some_rand/some_rand.go
+++ tst_workspace/some_rand/some_rand.go
@@ -1,7 +1,29 @@
 package somerand
 
-import "math/rand"
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+)
+
+type Message struct {
+	Name string
+	Body string
+	Time int64
+}
 
 func select_rand(n int) int {
 	return rand.Intn(n)
+}
+
+// * https://go.dev/blog/json
+func some_json() int {
+	m := Message{"Alice", "Hello", 1294706395881547000}
+	b, err := json.Marshal(m)
+	if err != nil {
+		// panic(err)
+		return -1
+	}
+	fmt.Printf("%s\n", b)
+	return 0
 }
